2d3c0456c607966592090ff105a371bda88e4ebe
“Yuvatheja” <skafroze2002@gmail.com>
2023-11-25 02:01:09 +0530

    home

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -23,13 +23,13 @@ export default function Nav() {
     <div className="w-screen ">
       <div className="nav-header h-[55px] bg-[#014584] flex items-center w-full px-24  justify-between ">
         <div className="flex items-center">
-          <Link to={'tel:1234567890'} className='no-underline'>
+          <Link to={"tel:1234567890"} className="no-underline">
             <i className="fa-solid fa-phone text-[#61B1D6] mr-3"></i>
             <span className="text-white mr-16 text-[15px] font-semibold">
               123-456-7890
             </span>
           </Link>
-          <Link to={'mailto:support@killerclean.com'} className='no-underline'>
+          <Link to={"mailto:support@killerclean.com"} className="no-underline">
             <i className="fa-solid fa-envelope text-[#61B1D6] mr-3"></i>
             <span className="text-white text-[15px] tracking-wide font-semibold">
               support@killerclean.com
@@ -37,27 +37,38 @@ export default function Nav() {
           </Link>
         </div>
         <div className=" flex items-center text-[24px]">
-          <Link className='no-underline text-white'><i className="fa-brands fa-facebook mr-6 hover:animate-pulse "></i></Link>
-          <Link className='no-underline text-white'><i className="fa-brands fa-instagram hover:animate-pulse"></i></Link>
+          <Link className="no-underline text-white">
+            <i className="fa-brands fa-facebook mr-6 hover:animate-pulse "></i>
+          </Link>
+          <Link className="no-underline text-white">
+            <i className="fa-brands fa-instagram hover:animate-pulse"></i>
+          </Link>
         </div>
       </div>
-    <Navbar fluid className="px-20 bg-[#F3FAFC] sticky top-0 z-10 w-full">
-      <Navbar.Brand href="/">
-        <img src={logo} className="h-6 sm:h-36" alt="Killer CLean Logo" />
-      </Navbar.Brand>
-        <Navbar.Toggle /> 
-      <Navbar.Collapse>
-        <Navbar.Link href="/">Home</Navbar.Link>
-        <Navbar.Link href="/services">Services</Navbar.Link>
-        <Navbar.Link href="/cleaningchecklist">Cleaning Checklist</Navbar.Link>
-        <Navbar.Link href="/customerlogin">Customer Login</Navbar.Link>
-        <Navbar.Link href="/contact">Contact</Navbar.Link>
-        <Navbar.Link href="/applynow">Apply Now</Navbar.Link>
-        {
-          (isMobile || isTablet)? <Navbar.Link href="/book">Book Online</Navbar.Link> :<Button as={Link} to={'/book'}>Book Online</Button>
-        }
-      </Navbar.Collapse>
-    </Navbar>
+
+      <Navbar fluid className="px-20  bg-[#F3FAFC]  w-full">
+        <Navbar.Brand href="/">
+          <img src={logo} className="h-6 sm:h-36" alt="Killer CLean Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link as={Link} to={'/'}>Home</Navbar.Link>
+          <Navbar.Link as={Link} to={'/services'}>Services</Navbar.Link>
+          <Navbar.Link href="/cleaningchecklist">
+            Cleaning Checklist
+          </Navbar.Link>
+          <Navbar.Link href="/customerlogin">Customer Login</Navbar.Link>
+          <Navbar.Link href="/contact">Contact</Navbar.Link>
+          <Navbar.Link href="/applynow">Apply Now</Navbar.Link>
+          {isMobile || isTablet ? (
+            <Navbar.Link href="/book">Book Online</Navbar.Link>
+          ) : (
+            <Button as={Link} to={"/book"}>
+              Book Online
+            </Button>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
---
+++ src/Components/Tired.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+export default function Tired() {
+  return (
+    <div className="bg-white mt-24 px-20 pt-20 w-full ">
+      <h1 className="text-center text-[#014584] text-[45px] mb-3 font-semibold">
+        Tired Of Cleaning By Yourself?
+      </h1>
+      <p className="text-center text-lg mx-56 text-[#014584]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Velit sed
+        ullamcorper morbi tincidunt ornare massa. Cursus risus at ultrices mi
+        tempus imperdiet nulla.:
+      </p>
+      <div className=" flex items-center w-full">
+        <div className="w-[50%] flex flex-col items-start p-8">
+          <h1 className="text-4xl text-[#014584] mb-4 font-semibold">
+            Consistent Quality
+          </h1>
+          <p className="text-[17px] text-[#014584] leading-8 text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit
+            sed ullamcorper morbi tincidunt ornare massa. Cursus risus at
+            ultrices mi tempus imperdiet nulla. Vulputate odio ut enim blandit
+            volutpat. Lacus luctus accumsan tortor posuere ac ut.
+          </p>
+        </div>
+        <div className="w-[50%] flex justify-end">
+          <img
+            src="https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-740-%C3%97-800-px-10-1.png"
+            alt="" className="w-[90%] p-7"
+          />
+        </div>
+      </div>
+      <div className="flex w-full flex-row-reverse items-center relative">
+        <div className="w-[50%] flex flex-col items-start p-8">
+          <h1 className="text-4xl text-[#014584] font-semibold mb-4">
+            Experienced and Background Checked Cleaners
+          </h1>
+          <p className="text-[#014584] text-[17px] text-left leading-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit
+            sed ullamcorper morbi tincidunt ornare massa. Cursus risus at
+            ultrices mi tempus imperdiet nulla. Vulputate odio ut enim blandit
+            volutpat. Lacus luctus accumsan tortor posuere ac ut.
+          </p>
+        </div>
+        <div className="w-[50%] flex justify-start">
+          <img
+            src="https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-740-%C3%97-800-px-7-1.png"
+            alt="" className="w-[90%] p-7"
+          />
+        </div>
+        <img
+          src="https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-design-2023-08-01T175153.298.png"
+          alt=""
+          className="absolute -left-[238px] top-20 h-52 -rotate-1"
+        />
+      </div>
+      <div className=" flex items-center w-full ">
+        <div className="w-[50%] flex flex-col items-start p-8">
+          <h1 className="text-4xl text-[#014584] font-semibold mb-4">
+            Online Booking & Payment and Responsive Customer Support
+          </h1>
+          <p className="text-[#014584] text-[17px] text-left mb-4 leading-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit
+            sed ullamcorper morbi tincidunt ornare massa. Cursus risus at
+            ultrices mi tempus imperdiet nulla. Vulputate odio ut enim blandit
+            volutpat. Lacus luctus accumsan tortor posuere ac ut.
+          </p>
+          <Button as={Link} to={"/book"}>
+            Schedule A Cleaning
+          </Button>
+        </div>
+        <div className="w-[50%] flex justify-end">
+          <img
+            src="https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-740-%C3%97-800-px-11-1.png"
+            alt="" className="w-[90%] p-7"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
