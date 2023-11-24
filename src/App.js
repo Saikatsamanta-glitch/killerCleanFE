9da4d445f6c821220e67c7829c053749e067931b
Rutzz <rutikabhang@gmail.com>
2023-11-24 19:11:26 +0530

    services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CustomFooter from './Components/CustomFooter'
 import Nav from './Components/Nav'
 import Home from './Pages/Home'
+import Services from './Pages/Services'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
     <Nav />
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/services' element={<Services />} />
     </Routes>
     <CustomFooter/>
     </BrowserRouter>
---
+++ src/Pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImageCard from '../Components/ImageCard';
+const Services = () => {
+    return (
+        <div>
+            <ImageCard/>
+        </div>
+    );
+}
+
+export default Services;
