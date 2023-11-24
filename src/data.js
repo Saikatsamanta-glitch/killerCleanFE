1c5508b268f90b08eab9b0f2d15905e733242c7f
Rutzz <rutikabhang@gmail.com>
2023-11-24 21:24:59 +0530

    services

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const servicesInfo = [
+  {
+    id: 1,
+    title: "Standard Cleaning",
+    img: "https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/1-8.png",
+    desc: "The standard cleaning is our most popular package. Keep your home neat & tidy.",
+  },
+  {
+    id: 2,
+    title: "Deep Cleaning",
+    img: "https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/2-3.png",
+    desc: "The deep clean is for busy homeowners who want their to-do list empty & house spotless.",
+  },
+  {
+    id: 3,
+    title: "Move In/Move Out Cleaning",
+    img: "https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-720-%C3%97-600-px-1-1.png",
+    desc: "The move-in/move-out cleaning will have your new or old home spotless and ready for moving day.",
+  },
+  {
+    id: 4,
+    title: "Post Construction Cleanup",
+    img: "https://sparklingclean.siterubix.com/wp-content/uploads/2023/09/Untitled-design-55-1.png",
+    desc: "Providing thorough and efficient cleaning solutions after construction projects, removing debris, dust, and ensuring a safe and pristine environment, leaving your site spotless and ready for use.",
+  },
+];
