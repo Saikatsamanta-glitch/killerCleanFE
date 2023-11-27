b808a985b8a18f0a04dce196129fa63e5a167409
Rutzz <rutikabhang@gmail.com>
2023-11-27 10:07:55 +0530

    services

--- src/Components/ImageCard.jsx
+++ src/Components/ImageCard.jsx
@@ -4,8 +4,8 @@ import { servicesInfo } from "../data";
 import { Link } from "react-router-dom";
 const ImageCard = () => {
   return (
-    <div className="bg-[#f3fafc] px-32">
-      <h1 className="text-[50px] text-center text-[#014584] font-semibold ">Our Services</h1>
+    <div className="bg-[#f3fafc] px-32 ">
+      <h1 className="text-[50px] text-center text-[#014584] font-semibold sm:max-md:text-[20px]">Our Services</h1>
       <p className="text-center text-lg mb-4 text-[#014584]">See our cleaning services below or view our complete <span className="underline text-[#046bd2]">cleaning checklist.</span></p>
       <div className="flex flex-col items-center bg-white h-[1100px] shadow-lg ">
         <div className="flex flex-wrap justify-center">
