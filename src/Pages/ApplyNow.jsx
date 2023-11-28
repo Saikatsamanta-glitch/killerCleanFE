606f00a980fb4415dfee6ee1e7ed39a98fa5f881
Rutzz <rutikabhang@gmail.com>
2023-11-28 14:15:10 +0530

    responsive

--- src/Pages/ApplyNow.jsx
+++ src/Pages/ApplyNow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ApplyNow = () => {
     return (
         <div className='text-center mx-10'>
-            <h1 className='md:max-xxl:text-[44px]  font-semibold text-[#014584] mt-16 mb-4'>Apply Now!</h1>
+            <h1 className='md:max-xxl:text-[44px] text-[34px] font-semibold text-[#014584] mt-16 mb-4'>Apply Now!</h1>
             <p className='text-[17px] mb-[124px] text-[#014584]'>Are you interested in working with us as a cleaning contractor? &nbsp; Fill out your contact information below.</p>
         </div>
     );
