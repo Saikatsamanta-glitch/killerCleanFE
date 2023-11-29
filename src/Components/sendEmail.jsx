35fb37ef7db5e529beb74e9686522cd364695e6a
Rutzz <rutikabhang@gmail.com>
2023-11-30 00:44:23 +0530

    email_sending

---
+++ src/Components/sendEmail.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lrzlb67', 'template_0qic7ra', form.current, 'public_key')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form className='mb-10' ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="to_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Date</label>
+      <input type="date" name='schedule_date'/>
+      <label>Time</label>
+      <input type="time" name='schedule_time'/>
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
