3078b654ebcf64f79339fa26d67fd3fe949da3c1
Shaik Afroze Sulthana <skafroze2002@gmail.com>
2023-12-06 20:26:51 +0530

    faqs

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -19,22 +19,6 @@ export default function FAQ() {
         </h1>
       </div>
       <div className="">
-        {/* <Accordion
-          multiple
-          className="w-full text-justify max-lg:p-4 lg:px-40 lg:py-10"
-        >
-          {faqs.map((q) => (
-            <AccordionTab
-              key={q.id}
-              header={q.que}
-              className="lg:mb-5 max-lg:my-3 text-[#014584] lg:max-xxl:text-xl xxl:text-xl"
-            >
-              <p className="m-0 text-black  lg:max-xxl:text-xl xxl:text-xl">
-                {q.ans}
-              </p>
-            </AccordionTab>
-          ))}
-        </Accordion> */}
         <Accordion allowZeroExpanded allowMultipleExpanded  className="w-full text-justify max-lg:p-4 lg:px-40 lg:py-10">
           {faqs.map((item) => (
             <AccordionItem key={item.id}>
